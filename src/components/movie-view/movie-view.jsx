68a8554224aebbf765873e2399e764dd72e487b1
lin <shilinxi@proton.me>
2024-01-02 15:07:04 +0000

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,20 @@
+const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Author: </span>
+        <span>{movie.author}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
+
+export { MovieView };
