a30a6ed94ee918f77882f824e0b34dd483c356ec
lin <shilinxi@proton.me>
2024-01-13 16:19:59 +0000

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: 'Pulp Fiction',
-      image: 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg',
-      director: 'Quentin Tarantino',
-      releaseYear: 1994,
-      genre: 'Crime',
-      description: 'Various interconnected stories of crime in Los Angeles.'
-    },
-    {
-      id: 2,
-      title: 'Avatar',
-      image: 'https://m.media-amazon.com/images/M/MV5BZDA0OGQxNTItMDZkMC00N2UyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_FMjpg_UY2902_.jpg',
-      director: 'James Cameron',
-      releaseYear: 2009,
-      genre: 'Fantasy',
-      description: 'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.'
-    },
-    {
-      id: 3,
-      title: 'The Shawshank Redemption',
-      image: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1200_.jpg',
-      director: 'Frank Darabont',
-      releaseYear: 1994,
-      genre: 'Drama',
-      description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
-    },
-  ]); //array destructure
-
+  const [movies, setMovies] = useState([]); //array destructure
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://openlibrary.org/search.json?q=star+wars")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("books from api:", data);
+    });
+  }, []);
 
   if (selectedMovie) {
     return (
