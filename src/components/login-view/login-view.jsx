68624f38e52eb3e9becfb5b571568bce2f00cd27
lin <lin7cb@outlook.com>
2024-01-16 12:29:47 +0000

    update sign up info

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,62 @@
-const LoginView = () => {
+import React from 'react';
+import { useState } from 'react';
+
+const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password,
+    };
+
+    fetch('https://movies-flix-lin-66267be64a83.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+    .then((response)=> response.json())
+    .then((data) => {
+      console.log('Login response:', data);
+      if (data.user) {
+        localStorage.setItem('user', JSON.stringify(data.user));
+        localStorage.setItem('token', data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert ('No such user');
+      }
+    }).catch((err)=> {
+      alert ('Something is broken..')
+    })
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" />
+        <input
+        type='text'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} required
+        />
       </label>
+
       <label>
         Password:
-        <input type="password" />
+        <input
+        type='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} required
+        />
       </label>
-      <button type="submit">Submit</button>
+
+      <button type='submit' method='post'>Submit</button>
     </form>
   );
 };
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,17 +2,28 @@ import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
+
   const [movies, setMovies] = useState([]); //array destructure
   const [selectedMovie, setSelectedMovie] = useState(null);
+  
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch('https://movies-flix-lin-66267be64a83.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+
+    fetch('https://movies-flix-lin-66267be64a83.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }})
     .then((response) => response.json())
     .then((data) => {
-      console.log("movies from api:", data);
+      console.log('movies from api:', data);
 
       const moviesFromApi = data.map((movie) => {
         return {
@@ -28,10 +39,20 @@ export const MainView = () => {
       setMovies(moviesFromApi);
 
     });
-  }, []);
+  }, [token]);
 
   if (!user) {
-    return <LoginView />;
+    return (
+      <>
+        <LoginView 
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
   }
 
   if (selectedMovie) {
@@ -57,6 +78,7 @@ export const MainView = () => {
           />
         );
       })}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 };
